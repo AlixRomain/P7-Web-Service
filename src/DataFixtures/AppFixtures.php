af78563221b6613f5744b6ad95b147c56384af3e
AlixRomain <romain.alix89@gmail.com>
2021-05-06 14:41:37 +0200

    Create a load method for push a fakes datas in DB

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Client;
+use App\Entity\Mobiles;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+    private $faker;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+        $this->faker = Factory::create();
+    }
+    public function load(ObjectManager $manager)
+    {
+
+        //FOR LOAD METHOD WITH DROP TABLE "symfony console doctrine:fixtures:load"
+        //FOR LOAD METHOD WITHOUT DROP TABLE "symfony console doctrine:fixtures:load --append"
+        $contenu_fichier_json = file_get_contents(__DIR__.'/datas.json');
+        $datas = json_decode($contenu_fichier_json, true);
+
+        foreach($datas['mobiles'] as $mobil ){
+            $mobile = new Mobiles();
+            $mobile->setName($mobil["name"])
+                    ->setDescription($mobil["description"])
+                    ->setPrice($mobil["price"]);
+            $manager->persist($mobile);
+        }
+
+        foreach($datas['clients'] as $client ){
+            $newClient = new Client();
+            $newClient->setName($client["name"]);
+            $newClient->setAdress($client["adress"]);
+            $manager->persist($newClient);
+            /*Build 1 user Admin users foreach client corporation*/
+            $userAdmin = new User();
+            $userAdmin->setUsername('Mr . '.$client["name"])
+                ->setEmail(strtolower($this->faker->word.'@'.$client["name"].'.com'))
+                ->setRoles(User::ROLE_CLIENT)
+                ->setPassword($this->encoder->encodePassword($userAdmin, "OpenClass21!"))
+                ->setAge($this->faker->numberBetween(18,90))
+                ->setCreatedAt($this->faker->dateTimeThisYear( 'now'))
+                ->setClient($newClient);
+            $manager->persist($userAdmin);
+
+            /*Build a 10 users simple for client corporation*/
+            for($i = 0; $i < 10; $i++){
+                $user = new User();
+                $user->setUsername('Client chez '.$newClient->getName())
+                    ->setEmail($this->faker->email)
+                    ->setAge($this->faker->numberBetween(18,90))
+                    ->setCreatedAt($this->faker->dateTimeThisYear( 'now'))
+                    ->setRoles(User::ROLE_USER)
+                    ->setPassword($this->encoder->encodePassword($user, "OpenClass21!"))
+                    ->setClient($newClient);
+                $manager->persist($user);
+            }
+        }
+        $AdminBileMo = new User();
+        $AdminBileMo->setUsername("Bile Mo")
+            ->setEmail("admin@admin.com")
+            ->setRoles(User::ROLE_ADMIN)
+            ->setPassword($this->encoder->encodePassword($AdminBileMo, "OpenClass21!"))
+            ->setCreatedAt($this->faker->dateTimeThisYear( 'now'))
+            ->setAge(25);
+        $manager->persist($AdminBileMo);
+        $manager->flush();
+    }
+}
+

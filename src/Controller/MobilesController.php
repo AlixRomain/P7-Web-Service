56f330f95bd077a0005a8d99da54b54fbf64502a
AlixRomain <romain.alix89@gmail.com>
2021-05-17 17:56:57 +0200

    build every routes with restrictions for entitys

--- src/Controller/MobilesController.php
+++ src/Controller/MobilesController.php
@@ -10,9 +10,7 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\ConstraintViolationList;
 
 class MobilesController extends AbstractFOSRestController
@@ -30,7 +28,6 @@ class MobilesController extends AbstractFOSRestController
      * @Rest\Get(
      *     path = "/api/mobiles",
      *     name = "all_mobiles_show",
-     *     requirements={"id"="\d+"}
      * )
      * @Rest\View(serializerGroups={"Default"})
      * @IsGranted("ROLE_USER")
@@ -58,12 +55,12 @@ class MobilesController extends AbstractFOSRestController
      /**
       * Add one mobile by admin
       * @Rest\Post(
-      *     path = "/api/admin/mobiles",
+      *     path = "/api/admin/mobile",
       *     name = "add_mobile",
       * )
       * @Rest\View(StatusCode = 201)
       * @ParamConverter(
-      *     "mobiles",
+      *     "mobile",
       *      converter="fos_rest.request_body",
       *      options={
       *         "validator" = {"groups" = "Create"}
@@ -100,12 +97,12 @@ class MobilesController extends AbstractFOSRestController
       /**
        * Update one mobile by admin
       * @Rest\Put(
-      *     path = "/api/admin/mobiles/{id}",
+      *     path = "/api/admin/mobile/{id}",
       *     name = "update_mobile",
       * )
       * @Rest\View(StatusCode = 201)
       * @ParamConverter(
-      *     "mobiles",
+      *     "mobile",
       *      converter="fos_rest.request_body",
       *      options={
       *         "validator" = {"groups" = "Create"}
@@ -143,8 +140,8 @@ class MobilesController extends AbstractFOSRestController
     /**
      * Delete one mobile by admin
      * @Rest\Delete(
-     *     path = "/api/delete-tools/{id}",
-     *     name = "tools_delete",
+     *     path = "/api/admin/mobile/{id}",
+     *     name = "delete_mobile",
      *     requirements={"id"="\d+"}
      * )
      * @Rest\View(StatusCode = 204)
